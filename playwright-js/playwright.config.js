5654da97225a0125eeb7587b58af1414c810bf92
adevivanco <andy.dv.cm@gmail.com>
2022-07-23 17:33:40 -0500

    first commit to 'playwright-js-codegen-examle' branch

--- playwright-js/playwright.config.js
+++ playwright-js/playwright.config.js
@@ -33,6 +33,8 @@ const config = {
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
+
+  
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
@@ -41,7 +43,10 @@ const config = {
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+   // trace: 'on-first-retry',
+
+    trace: 'on'
+
   },
 
   /* Configure projects for major browsers */
--- playwright-js/tests/e2e-tests.spec.js
+++ playwright-js/tests/e2e-tests.spec.js
@@ -15,27 +15,41 @@ test('homepage has React App in title and Add Workout button', async ({ page }) 
   
   });
 
-  // assertions and locators
-  test('homepage has header with Workout Buddy link', async ({ page }) => {
+// assertions and locators
+test('homepage has header with Workout Buddy link', async ({ page }) => {
+
     await page.goto('http://localhost:3000/');
-    
+
     // create a locator
     const firsHeaderLinkLocator = page.locator('text=Workout Buddy');
-
     // Expect an attribute "to be strictly equal" to the value.
     await expect(firsHeaderLinkLocator).toHaveAttribute('href', '');
-  
-  });
-  
-  // using test hooks
-  test.describe("navigation", () => {
+
+    });
+
+    // using test hooks
+    test.describe("navigation", () => {
     test.beforeEach(async ({ page }) => {
-      // Go to the starting url before each test.
-      await page.goto("http://localhost:3000/");
+        // Go to the starting url before each test.
+        await page.goto("http://localhost:3000/");
     });
-  
+
     test("main navigation", async ({ page }) => {
-      // Assertions use the expect API.
-      await expect(page).toHaveURL("http://localhost:3000/");
+        // Assertions use the expect API.
+        await expect(page).toHaveURL("http://localhost:3000/");
     });
-  });
+});
+
+// test generated with playwright codegen using following command line:
+//   npx playwright codegen wikipedia.org
+//
+test('wikipedia codegen test', async ({ page }) => {
+    // Go to https://www.wikipedia.org/
+    await page.goto('https://www.wikipedia.org/');
+    // Click strong:has-text("English")
+    await page.locator('strong:has-text("English")').click();
+    await expect(page).toHaveURL('https://en.wikipedia.org/wiki/Main_Page');
+    // Click text=View source
+    await page.locator('text=View source').click();
+    await expect(page).toHaveURL('https://en.wikipedia.org/w/index.php?title=Main_Page&action=edit');
+});
