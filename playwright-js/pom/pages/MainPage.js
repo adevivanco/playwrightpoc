85c41beee4aada8594f4cf410b15a40276a3909e
adevivanco <andy.dv.cm@gmail.com>
2022-07-23 20:31:46 -0500

    finish first set of tests inside describe

--- playwright-js/pom/pages/MainPage.js
+++ playwright-js/pom/pages/MainPage.js
@@ -1,27 +1,30 @@
 const { expect } = require('@playwright/test');
+const { url } = require('inspector');
 const { BasePage } = require('./BasePage');
 
 exports.MainPage = class MainPage extends BasePage {
 
   constructor(page) {
     super();
-   this.page = page;
+    this.page = page;
     this.firsHeaderLinkLocator =  page.locator('text=Workout Buddy');
     this.workoutButtonLocator = page.locator('div.home > form.create > button');
-  }
-
-  async validateWorkoutBuddyHeaderLinkExistance () {
-    await expect(this.firsHeaderLinkLocator).toHaveAttribute('href', '');
-  }
-
-  async validateCreateWorkoutExistance (buttonName) {
-    // Expect a locator to have text "to be strictly equal" to the value.
-    await expect(this.workoutButtonLocator).toHaveText(buttonName);
   }
 
   async goto() {
     await this.page.goto('http://localhost:3000');
   }
 
+  async validateWorkoutBuddyHeaderLinkExistance () {
+    await expect(this.firsHeaderLinkLocator).toHaveAttribute('href', '');
+  }
+
+  async validateCreateWorkoutExistance(buttonName) {
+    await expect(this.workoutButtonLocator).toHaveText(buttonName);
+  }
+
+  async validateUrlEqualsTo(url) {
+    await expect(this.page).toHaveURL(url);
+  }
   
 }
--- playwright-js/tests/e2e-tests.spec.js
+++ playwright-js/tests/e2e-tests.spec.js
@@ -1,38 +1,33 @@
 const { test, expect } = require('@playwright/test');
 const { MainPage } = require('../pom/pages/MainPage');
+
 // first test
-test('homepage has React App in title and Add Workout button', async ({ page }) => {
+
+test.describe('end to end tests of workouts app', () => {
+    test.beforeEach(async ({ page }) => {
+        const mainPage = new MainPage(page);
+        // Go to the starting url before each test.
+        await mainPage.goto();
+    });
+
+    test('homepage has React App in title and Add Workout button', async ({ page }) => {
+        const mainPage = new MainPage(page);
+        await mainPage.validatePageTitleEquals(/React App/);
+        await mainPage.validateCreateWorkoutExistance('Add Workout');
+     });
+
+        // assertions and locators
+    test('homepage has header with Workout Buddy link', async ({ page }) => {
+        const mainPage = new MainPage(page);
+        await mainPage.validateWorkoutBuddyHeaderLinkExistance();
+    });
     
-   const mainPage = new MainPage(page);
-   await mainPage.goto();
-   await mainPage.validatePageTitleEquals(/React App/);
-   await mainPage.validateCreateWorkoutExistance('Add Workout');
-  
-  });
+    test('homepage has correct URL', async ({ page }) => {
+        const mainPage = new MainPage(page);
+        await mainPage.validateWorkoutBuddyHeaderLinkExistance();
+    });
+    
 
-// // assertions and locators
-// test('homepage has header with Workout Buddy link', async ({ page }) => {
-
-//     await page.goto('http://localhost:3000/');
-
-//     // create a locator
-//     const firsHeaderLinkLocator = page.locator('text=Workout Buddy');
-//     // Expect an attribute "to be strictly equal" to the value.
-//     await expect(firsHeaderLinkLocator).toHaveAttribute('href', '');
-
-// });
-
-//     // using test hooks
-// test.describe("navigation", () => {
-//     test.beforeEach(async ({ page }) => {
-//         // Go to the starting url before each test.
-//         await page.goto("http://localhost:3000/");
-//     });
-
-//     test("main navigation", async ({ page }) => {
-//         // Assertions use the expect API.
-//         await expect(page).toHaveURL("http://localhost:3000/");
-//     });
-// });
+});
 
 
